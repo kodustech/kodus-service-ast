0b621649315d1af40c73b92ac6083612eba4c864
Jairo Litman <130161309+jairo-litman@users.noreply.github.com>
2025-07-10 15:31:49 -0300

    merge services

--- src/main.ts
+++ src/main.ts
@@ -14,13 +14,11 @@ import { PinoLoggerService } from './core/infrastructure/adapters/services/logge
 async function bootstrap() {
     const containerName = getEnvVariableOrExit('CONTAINER_NAME');
     const grpcASTPort = getEnvVariableAsNumberOrExit('API_AST_PORT');
-    const grpcTaskPort = getEnvVariableAsNumberOrExit('API_TASK_PORT');
     const healthPort = getEnvVariableAsNumberOrExit('API_HEALTH_PORT');
 
     /* ------------ validação simples de intervalo ---------------- */
     for (const [name, value] of Object.entries({
         grpcASTPort,
-        grpcTaskPort,
         healthPort,
     })) {
         if (value < 1 || value > 65535) {
@@ -29,14 +27,8 @@ async function bootstrap() {
         }
     }
 
-    if (
-        grpcASTPort === healthPort ||
-        grpcTaskPort === healthPort ||
-        grpcASTPort === grpcTaskPort
-    ) {
-        console.error(
-            'API_AST_PORT, API_TASK_PORT and API_HEALTH_PORT cannot be the same',
-        );
+    if (grpcASTPort === healthPort) {
+        console.error('API_AST_PORT and API_HEALTH_PORT cannot be the same');
         process.exit(1);
     }
     /* ------------------------------------------------------------ */
@@ -55,34 +47,17 @@ async function bootstrap() {
             transport: Transport.GRPC,
             options: {
                 url: `0.0.0.0:${grpcASTPort}`,
-                package: 'kodus.ast.v3',
-                protoPath: resolve(
-                    cwd(),
-                    'node_modules/@kodus/kodus-proto/kodus/ast/v3/analyzer.proto',
-                ),
-                credentials: ServerCredentials.createInsecure(), // plaintext
-                loader: {
-                    includeDirs: [
-                        join(cwd(), 'node_modules/@kodus/kodus-proto'),
-                    ],
-                },
-            },
-        },
-        {
-            inheritAppConfig: true,
-        },
-    );
-
-    app.connectMicroservice<MicroserviceOptions>(
-        {
-            transport: Transport.GRPC,
-            options: {
-                url: `0.0.0.0:${grpcTaskPort}`,
-                package: 'kodus.task.v1',
-                protoPath: resolve(
-                    cwd(),
-                    'node_modules/@kodus/kodus-proto/kodus/task/v1/manager.proto',
-                ),
+                package: ['kodus.ast.v3', 'kodus.task.v1'],
+                protoPath: [
+                    resolve(
+                        cwd(),
+                        'node_modules/@kodus/kodus-proto/kodus/ast/v3/analyzer.proto',
+                    ),
+                    resolve(
+                        cwd(),
+                        'node_modules/@kodus/kodus-proto/kodus/task/v1/manager.proto',
+                    ),
+                ],
                 credentials: ServerCredentials.createInsecure(), // plaintext
                 loader: {
                     includeDirs: [
@@ -103,7 +78,7 @@ async function bootstrap() {
     await app.startAllMicroservices();
     console.log(`HTTP health => ${containerName}:${healthPort}`);
     console.log(`gRPC AST    => ${containerName}:${grpcASTPort}`);
-    console.log(`gRPC Task   => ${containerName}:${grpcTaskPort}`);
+    console.log(`gRPC Task   => ${containerName}:${grpcASTPort}`);
 
     /* 4. Avise PM2 que o processo está pronto (wait_ready) */
     if (typeof process.send === 'function') {
